49d38687d5bc694c506a4d9d4d1896325593784b
aditya-eko <ekoagusaditya@gmail.com>
2019-09-01 19:15:57 +0700

    menambahkan semua file

---
+++ budgeting.py
@@ -0,0 +1,47 @@
+modalAwal=50000000
+
+
+def pembelian(up):
+    print("toko ini membeli dengan harga",up)
+
+def penjualan(up):
+    print("barang ini di jual dengan harga",up)
+
+def keuntungan_peritem(penjualan,pembelian):
+    total=penjualan-pembelian
+    return total
+
+
+def saldoAkhir(keuntungan):
+    global modalAwal
+    total=modalAwal+keuntungan
+    return total
+
+
+def labarugi(keuntungan):
+    global modalAwal
+    if modalAwal<keuntungan:
+        laba=keuntungan-modalAwal
+        return laba
+    elif modalAwal>keuntungan:
+        rugi=modalAwal-keuntungan
+        return rugi
+
+    elif modalAwal==keuntungan:
+        impas=modalAwal==keuntungan
+        return impas
+    else:
+        print("terjadi kesalahan penghitungan")
+
+
+
+pembelian(10000)
+penjualan(15000)
+untung=keuntungan_peritem(15000,10000)
+print(untung)
+saldoAkhir(70000000)
+totalsaldo=saldoAkhir(70000000)
+print(totalsaldo)
+
+labaataurugi=labarugi(80000000)
+print(labaataurugi)
